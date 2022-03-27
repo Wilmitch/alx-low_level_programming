9b4bad31a5348ac4a22d099121582e632c1dae79
Wilmitch <mwangimwilfred@gmail.com>
2022-03-27 05:26:50 -0700

    edited 101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,8 @@
 
 
 /**
- * main - lists all the natural numbers below 1024 (excluded) that are multiples of 3 or 5.
+ * main - lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
  *
  * Return: Always 0.
  */
