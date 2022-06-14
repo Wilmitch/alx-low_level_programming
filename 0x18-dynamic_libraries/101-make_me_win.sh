6fea0bfcc44a9d0d69e489411cfc811b1f4b4c43
wilmitch <mwangimwilfred@gmail.com>
2022-06-14 03:04:10 +0000

    dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -P /tmp https://github.com/Wilmitch/alx-low_level_programming \
-	/tree/master/0x18-dynamic_libraries/101-make_me_win.sh
+/blob/master/0x18-dynamic_libraries/101-make_me_win.sh
 export LD_PRELOAD=/tmp/libmask.so
