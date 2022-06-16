b39d1b877e03ced2254689a1a0f7ac0c6962d6b7
wilmitch <mwangimwilfred@gmail.com>
2022-06-16 01:25:32 +0000

    corrected 101 make me win.sh file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Wilmitch/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.sh
+wget -P /tmp https://github.com/Wilmitch/alx-low_level_programming\
+	/blob/master/0x18-dynamic_libraries/101-make_me_win.sh
 export LD_PRELOAD=/tmp/libmask.so
